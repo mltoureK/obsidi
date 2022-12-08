31a1b5c023fcdabea05ffbe5c9c8fd1c12d96e12
Moos Toure <ml.toure1000@gmail.com>
2022-12-08 05:51:25 -0500

    filter package

--- Feedapp/src/main/java/com/btpn/filter/JWTAuthorizationFilter.java
+++ Feedapp/src/main/java/com/btpn/filter/JWTAuthorizationFilter.java
@@ -50,6 +50,8 @@ public class JWTAuthorizationFilter implements Filter {
         }
         filterChain.doFilter(req, res);
     }
+    //save
+    
 
     private boolean isAuthenticationRequired(String url) {
         String[] excludedUrlList = excludedUrls.split(",");
